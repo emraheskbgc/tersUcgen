06942adb21fd081b7da491fde147dcd30c937536
Emrah Eskibagci <emraheskibagci595@gmail.com>
2022-09-22 18:41:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Bir N Sayısı Giriniz = ");
+        int n = input.nextInt();
+
+        for (int i=n; i>=0; i--){
+            for (int j=n-i; j>=1; j--){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=(2*i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
